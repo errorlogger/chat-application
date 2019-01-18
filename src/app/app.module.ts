d8d43f4a4400fb0c9436b222611ee08ec4d486aa
errorlogger <jmpourchel@hotmail.com>
2019-01-18 18:56:34 +0100

    premier commit e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { AuthModule } from './modules/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthRoutingModule } from './modules/auth-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
